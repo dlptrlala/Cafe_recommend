1f2bf1e2257ae3dfd024e109377b846d41057010
dlptrlala <margaretanovianti79@gmail.com>
2024-12-12 23:11:42 +0700

    benerin bagian session dan auth php

--- config/auth.php
+++ config/auth.php
@@ -14,8 +14,8 @@ return [
     */
 
     'defaults' => [
-        'guard' => env('AUTH_GUARD', 'web'),
-        'passwords' => env('AUTH_PASSWORD_BROKER', 'users'),
+        'guard' => 'users',
+        'passwords' => 'users',
     ],
 
     /*
@@ -36,10 +36,15 @@ return [
     */
 
     'guards' => [
-        'web' => [
+        'users' => [
             'driver' => 'session',
             'provider' => 'users',
         ],
+
+        // 'admins' => [
+        //     'driver' => 'session',
+        //     'provider' => 'admins',
+        // ],
     ],
 
     /*
@@ -62,12 +67,11 @@ return [
     'providers' => [
         'users' => [
             'driver' => 'eloquent',
-            'model' => env('AUTH_MODEL', App\Models\User::class),
+            'model' => App\Models\User::class,
         ],
-
-        // 'users' => [
-        //     'driver' => 'database',
-        //     'table' => 'users',
+        // 'karyawans' => [
+        //     'driver' => 'eloquent',
+        //     'model' => App\Models\Karyawan::class,
         // ],
     ],
 
